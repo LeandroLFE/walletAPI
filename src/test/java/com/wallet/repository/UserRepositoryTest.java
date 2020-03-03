e8ec0cb32924bc84beb75f147f99c0d89095463f
LeandroLFE <lfe.leandro@gmail.com>
2020-03-03 15:16:10 -0300

    Finished User Repository

---
+++ src/test/java/com/wallet/repository/UserRepositoryTest.java
@@ -0,0 +1,70 @@
+package com.wallet.repository;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Optional;
+
+import com.wallet.entity.User;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+/**
+ * UserRepositoryTest
+ */
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@ActiveProfiles("test")
+public class UserRepositoryTest {
+
+    private static final String EMAIL = "email@teste.com";
+
+    @Autowired
+    UserRepository repository;
+
+    @BeforeEach
+    public void setUp(){
+        User u = new User();
+        u.setName("Set up User");
+        u.setPassword("Senha123");
+        u.setEmail(EMAIL);
+
+        repository.save(u);
+    }
+
+    @AfterEach
+    public void tearDown(){
+
+        repository.deleteAll();
+    
+    }
+
+    @Test
+    public void testSave(){
+        User u = new User();
+        u.setName("Teste");
+        u.setPassword("123456");
+        u.setEmail("teste@teste.com");
+
+        User response = repository.save(u);
+
+        assertNotNull(response);
+    }
+
+    public void testFindByEmail(){
+        Optional<User> response = repository.findByEmailEquals(EMAIL);
+
+        assertTrue(response.isPresent());
+
+        assertEquals(response.get().getEmail(), EMAIL);
+    }
+
+}
